965c6131e11e95be3c30ba4546e4064be5183d75
djolivier <olivieer@users.noreply.github.com>
2014-03-13 21:14:44 +0100

    nozomi: cm.mk: several changes

--- cm.mk
+++ cm.mk
@@ -1,3 +1,4 @@
+# Inherit from AOSP device configuartion
 $(call inherit-product, device/sony/nozomi/full_nozomi.mk)
 
 # Inherit CM common GSM stuff.
@@ -12,6 +13,10 @@ TARGET_SCREEN_WIDTH := 720
 
 # Inherit CM common Phone stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
+CM_BUILDTYPE := djolivier
+PRODUCT_RELEASE_NAME := CyanogenMod-10.0-LB
+BUILD_ID := CyanogenMod-10.0-LB-$(shell date -u +%Y%m%d)
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT26i BUILD_FINGERPRINT=SEMC/LT26i_1257-6919/LT26i:4.1.2/6.2.B.0.200/N7__zg:user/release-keys PRIVATE_BUILD_DESC="LT26i-user 4.1.2 6.2.B.0.200 N7__zg test-keys"
 
